4ec797f02d075a06d66473bfde03863f54ef35e7
roksiniHD <hasan.nazarovhd@gmail.com>
2021-01-23 02:05:03 +0900

    first commit

---
+++ listings/choices.py
@@ -0,0 +1,38 @@
+bedroom_choices ={
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+}
+
+price_choices = {
+    '10000' : '$10,000',
+    '20000' : '$20,000',
+    '30000' : '$30,000',
+    '40000' : '$40,000',
+    '50000' : '$50,000',
+    '80000' : '$80,000',
+    '100000' : '$100,000',
+    '200000' : '$200,000',
+    '400000' : '$400,000',
+    '500000' : '$500,000',
+}
+
+state_choices ={
+    'Andijan' : 'Andijan',
+    'BUKH': 'Bukhara',
+    'FER': 'Fergana',
+    'JIZ':'Jizzakh',
+    'KASH': 'Kashkadarya',
+    'KHO': 'Khorezm',
+    'Namangan': 'Namangan',
+    'NAV': 'Navoi',
+    'RK': 'Republic of Karakalpakstan',
+    'SAM': 'Samarkand',
+    'SIR': 'Sirdarya',
+    'SUR': 'Surkhandarya',
+    'TASH': 'Tashkent',
+}
